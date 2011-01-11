3fc4f598f1ee56170e6efafd5c9b28cd376d2437
alessandro.calzavara <alessandro.calzavara@a2462d27-eda1-9003-ddea-35059cd9ebe4>
2011-01-11 14:38:33 +0000

    migliorata la funzione di stampa...

--- trunk/src/ia/vrptw/VRPTWDrawingSolution.java
+++ trunk/src/ia/vrptw/VRPTWDrawingSolution.java
@@ -3,18 +3,17 @@ package ia.vrptw;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.awt.geom.Rectangle2D;
+import java.text.DecimalFormat;
 import java.util.Random;
 
-import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageOutputStream;
 import javax.swing.*;
 
 public class VRPTWDrawingSolution {
 
+	static final int BORDER_LEFT = 10;
+	static final int BORDER_TOP = 10;
+	
 	VRPTWSolution _solution;
 	static Color standard_colors[] = new Color[50];
 	static {
@@ -34,7 +33,7 @@ public class VRPTWDrawingSolution {
 		System.out.println("* ottimizzazione terminata *");
 		// solution.show();
 		
-		solver.printSolution(solution, "prova.png");
+		solver.printSolution(solution, solution, "prova.png");
 		
 //		SwingUtilities.invokeLater(new Runnable() {
 //			public void run() {
@@ -68,17 +67,14 @@ public class VRPTWDrawingSolution {
 //		});
 	}
 
-	public VRPTWDrawingSolution(VRPTWSolution solution) {
-		_solution = solution;
-	}
-
 	static class DrawingArea extends JPanel {
 
-		VRPTWSolution _solution;
+		VRPTWSolution _solution, _initial_solution;
 
-		public DrawingArea(VRPTWSolution solution) {
+		public DrawingArea(VRPTWSolution solution, VRPTWSolution initial_solution) {
 			setBackground(Color.WHITE);
 			_solution = solution;
+			_initial_solution = initial_solution;
 		}
 
 		public void paintComponent(Graphics g) {
@@ -90,18 +86,19 @@ public class VRPTWDrawingSolution {
 			double zoom = 8;
 			Color colors[] = standard_colors;
 
+			// stampo customers e tragitti
 			int c = 0;
 			for (VRPTWRoute route : _solution.routes) {
 
-				double pre_x = route._warehouse._position_x * zoom;
-				double pre_y = route._warehouse._position_y * zoom;
+				double pre_x = BORDER_LEFT + route._warehouse._position_x * zoom;
+				double pre_y = BORDER_TOP + route._warehouse._position_y * zoom;
 
 				double x = pre_x;
 				double y = pre_y;
 				
 				for (VRPTWCustomer customer : route.customers) {
-					x = customer._position_x * zoom;
-					y = customer._position_y * zoom;
+					x = BORDER_LEFT + customer._position_x * zoom;
+					y = BORDER_TOP +  customer._position_y * zoom;
 
 					Ellipse2D circle = new Ellipse2D.Double();
 					circle.setFrameFromCenter(x, y, x + 2, y + 2);
@@ -125,7 +122,30 @@ public class VRPTWDrawingSolution {
 				c++;
 
 			}
-
+			
+			// disegno le due barre
+			Rectangle2D barra_km = new Rectangle2D.Double(900.0, 550-500*(_solution.totalTravelDistance()/_initial_solution.totalTravelDistance()), 25.0, 500*(_solution.totalTravelDistance()/_initial_solution.totalTravelDistance()));
+			g2.setColor(Color.blue);
+			g2.setPaint(Color.blue);
+			g2.draw(barra_km);
+			g2.fill(barra_km);
+			g.drawString("km", 903, 570);
+			
+			double altezza_barra_mezzi = (double)_solution.routes.size()/(double)_initial_solution.routes.size();
+			Rectangle2D barra_mezzi = new Rectangle2D.Double(950.0, 550-500*(altezza_barra_mezzi), 25.0, 500*(altezza_barra_mezzi));
+			g2.setColor(Color.green);
+			g2.setPaint(Color.green);
+			g2.draw(barra_mezzi);
+			g2.fill(barra_mezzi);
+			g.drawString("mezzi", 944, 570);
+			
+			// riepilogo km e mezzi in basso a destra
+			g2.setColor(Color.black);
+			g2.setPaint(Color.black);
+			DecimalFormat twoDForm = new DecimalFormat("#.##");
+			g.drawString("km: "+twoDForm.format(_solution.totalTravelDistance()), 903, 635);
+			g.drawString("mezzi: "+_solution.routes.size(), 903, 650);
+			
 		}
 
 	}
