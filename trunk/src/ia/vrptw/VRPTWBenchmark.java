ef6f7e236e6441db2ca15f14db8f663ef8942575
alessandro.calzavara <alessandro.calzavara@a2462d27-eda1-9003-ddea-35059cd9ebe4>
2011-01-21 17:01:56 +0000

    Aggiunti alcuni problemi R2 e RC2

--- trunk/src/ia/vrptw/VRPTWBenchmark.java
+++ trunk/src/ia/vrptw/VRPTWBenchmark.java
@@ -25,27 +25,34 @@ public class VRPTWBenchmark {
 									new VRPTWProblem("C203", 700), new VRPTWProblem("C204", 700),
 									new VRPTWProblem("C205", 700), new VRPTWProblem("C206", 700),
 									new VRPTWProblem("C207", 700), new VRPTWProblem("C208", 700)
-				};
+									};
 
 		VRPTWProblem r1_problems[] = {
-									new VRPTWProblem("R101", 200),
-									new VRPTWProblem("R102", 200), new VRPTWProblem("R103", 200),
-									new VRPTWProblem("R104", 200), new VRPTWProblem("R105", 200),
-									new VRPTWProblem("R106", 200), new VRPTWProblem("R107", 200),
-									new VRPTWProblem("R108", 200), new VRPTWProblem("R109", 200),
-									new VRPTWProblem("R110", 200), new VRPTWProblem("R111", 200),
-									new VRPTWProblem("R112", 200)
+									new VRPTWProblem("R101", 200), new VRPTWProblem("R102", 200),
+									new VRPTWProblem("R103", 200), new VRPTWProblem("R104", 200), 
+									new VRPTWProblem("R105", 200), new VRPTWProblem("R106", 200), 
+									new VRPTWProblem("R107", 200), new VRPTWProblem("R108", 200), 
+									new VRPTWProblem("R109", 200), new VRPTWProblem("R110", 200),
+									new VRPTWProblem("R111", 200), new VRPTWProblem("R112", 200)
 									};
 
+		VRPTWProblem r2_problems[] = {
+									new VRPTWProblem("R201", 1000)
+									};
+		
 		VRPTWProblem rc1_problems[] = {
-									new VRPTWProblem("RC101", 200),
-									new VRPTWProblem("RC102", 200), new VRPTWProblem("RC103", 200),
-									new VRPTWProblem("RC104", 200), new VRPTWProblem("RC105", 200),
-									new VRPTWProblem("RC106", 200), new VRPTWProblem("RC107", 200),
-									new VRPTWProblem("RC108", 200)
+									new VRPTWProblem("RC101", 200), new VRPTWProblem("RC102", 200), 
+									new VRPTWProblem("RC103", 200), new VRPTWProblem("RC104", 200), 
+									new VRPTWProblem("RC105", 200), new VRPTWProblem("RC106", 200), 
+									new VRPTWProblem("RC107", 200), new VRPTWProblem("RC108", 200)
 									};
 
-		for (VRPTWProblem p : c2_problems) {
+		VRPTWProblem rc2_problems[] = {
+									new VRPTWProblem("RC201", 1000), new VRPTWProblem("RC202", 1000),
+									new VRPTWProblem("RC205", 1000)
+									};
+
+		for (VRPTWProblem p : rc1_problems) {
 			VRPTWSolver solver = new VRPTWSolver();
 			//solver.activateDrawingSolutionsMode();
 			System.out.println("<< INIZIO OTTIMIZZAZIONE "+p.getInstanceName()+" >> ");
